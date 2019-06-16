e7f5b19b2f514b2f412d6438ac34af8acfa63071
Igor Tikhanovich <tikhanovich.89@gmail.com>
2019-06-16 13:08:09 +0300

    laravel vue crud

---
+++ resources/js/api/users.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const client = axios.create({
+    baseURL: '/api',
+});
+
+export default {
+    all(page) {
+        const params = { page };
+
+        return client.get('users', { params });
+    },
+    find(id) {
+        return client.get(`users/${id}`);
+    },
+    update(id, data) {
+        return client.put(`users/${id}`, data);
+    },
+    delete(id) {
+        return client.delete(`users/${id}`);
+    },
+};
